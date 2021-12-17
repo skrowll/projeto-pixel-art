8c2fb2eb6ca6faca25ba98368804d274f0cf4be0
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2021-12-17 13:25:31 -0300

    Requisitos 2 e 3 Refatorados OK

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// function createColors(colors) {
-//     document.createElement('div')
-//     document.body.appendChild()
-//     let palette = document.getElementById('color-palette');
-// }
-// let colors = ['black', 'red', 'green', 'blue'];
-
 let header = document.createElement('header');
 document.body.appendChild(header);
 
@@ -13,4 +6,18 @@ headerH1.id = 'title';
 header.appendChild(headerH1);
 headerH1.innerHTML = "Paleta de Cores";
 
+let colorPalette = document.createElement('section');
+colorPalette.id = 'color-palette';
+document.body.appendChild(colorPalette);
+
+function createColors(colors) {
+    for (index = 0; index < colors.length; index += 1) {
+        let color = document.createElement('div');
+        color.className = 'color';
+        color.style.backgroundColor = colors[index];
+        colorPalette.appendChild(color);
+    }
+}
+let colors = ['black', 'red', 'green', 'blue'];
+createColors(colors);
 
