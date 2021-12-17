e293130b6332daa0859df82278a054d80de4b1e6
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2021-12-17 12:56:09 -0300

    Requisito 1 Refatorado OK

---
+++ script.js
@@ -0,0 +1,16 @@
+// function createColors(colors) {
+//     document.createElement('div')
+//     document.body.appendChild()
+//     let palette = document.getElementById('color-palette');
+// }
+// let colors = ['black', 'red', 'green', 'blue'];
+
+let header = document.createElement('header');
+document.body.appendChild(header);
+
+let headerH1 = document.createElement('h1');
+headerH1.id = 'title';
+header.appendChild(headerH1);
+headerH1.innerHTML = "Paleta de Cores";
+
+
